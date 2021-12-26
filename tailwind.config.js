728c319e443b50e39c875265bde5afec54e6bcde
goodwillsandy <sandeep.kumar@smallcase.com>
2021-12-27 00:31:31 +0530

    Added tailwind css to project

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    spacing: {
+      4: "4px",
+      8: "8px",
+      12: "12px",
+      16: "16px",
+      20: "20px",
+      24: "24px",
+      28: "28px",
+      32: "32px",
+      36: "36px",
+      56: "56px",
+    },
+    colors: {
+      primary: "#1f2937",
+      secondary: "#f5f5f5",
+    },
+  },
+  plugins: [],
+}
