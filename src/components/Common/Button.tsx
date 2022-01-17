e8e98c5a165254b15ad837f4fcb51d13ec73e98c
goodwillsandy <sandeep.kumar@smallcase.com>
2022-01-17 15:39:27 +0530

    Added support for link in button

--- src/components/Common/Button.tsx
+++ src/components/Common/Button.tsx
@@ -1,32 +1,39 @@
 import React from 'react'
 
 interface Props {
-  children: React.ReactNode,
-  type: 'primary' | 'secondary',
-  size: 'large' | 'medium' | 'small',
-  fullWidth?: boolean,
+  children: React.ReactNode;
+  type: 'primary' | 'secondary';
+  size: 'large' | 'medium' | 'small';
+  fullWidth?: boolean;
+  href?: string;
 }
 
 function Button(props: Props): JSX.Element {
   const buttonProps = {
     className: `btn btn-${props.type} btn-${props.size} ${props.fullWidth ? 'btn-full-width' : ''}`,
   };
+
+  const Wrapper = props.href ? 'a' : 'button';
+
   return (
     <>
       <style jsx>{`
           .btn-primary {
-            @apply bg-primary text-lightPrimary rounded;
+            @apply bg-dark text-light;
+            display: inline-block;
+            min-width: '100px';
+            width: fit-content;
           }
           .btn-secondary {
             @apply text-primary border-solid border-2 border-primary rounded w-full;
           }
           .btn-medium {
-            @apply px-8 py-8;
+            @apply px-16 py-8;
           }
       `}</style>
-      <button {...buttonProps}>
+      <Wrapper {...buttonProps}>
         {props.children}
-      </button>
+      </Wrapper>
     </>
   )
 }
